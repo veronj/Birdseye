12f519a0d2dce3ef472ce86538f476fac833a6aa
veronj <veron_j@etna-alternance.net>
2019-01-23 20:06:49 +0100

    change layout

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">XXXXX</div>
+                    <div class="card-header">Your project</div>
     
                     <div class="card-body">
                         <form method="POST" action="/projects">
