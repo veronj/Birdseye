89f0ed0d220bb882092f592e95db6a5d13c23fcb
veronj <veron_j@etna-alternance.net>
2019-01-24 23:39:14 +0100

    task model

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -24,4 +24,13 @@ class ProjectTest extends TestCase
         $project = factory('App\Project')->create();
         $this->assertInstanceOf('App\User', $project->owner);
     }
+
+    /** @test  */
+    public function it_can_add_a_task()
+    {
+        $project = factory('App\Project')->create();
+        $project->addTask('Test task');
+
+        $this->assertCount(1, $project->tasks);
+    }
 }
